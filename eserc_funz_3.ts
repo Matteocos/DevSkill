581cb2f042a7bd73cffee22eed22b58c7cbd3bb8
Matteo Cosenza <matteocosenza12@gmail.com>
2022-04-21 17:09:16 +0200

    Aggiunto esercizio funzioni 3

--- eserc_funz_3.ts
+++ eserc_funz_3.ts
@@ -2,25 +2,24 @@ function numeroMaggiore(input1: number, input2: number, input3: number): string 
 
 
     if (input1 > input2 && input1 > input3) {
-        return console.log("il numero più alto è il primo inserito");
+        return "il numero più alto è il primo inserito";
     }
     else if (input2 > input1 && input2 > input3) {
-        return console.log("il numero più alto è il secondo inserito");
+        return "il numero più alto è il secondo inserito";
     }
     else if (input3 > input1 && input3 > input2) {
-        return console.log("il numero più alto è il terzo inserito");
-    }
-    else {
-        return console.log("due o più numeri inseriti sono uguali");
+        return "il numero più alto è il terzo inserito";
     }
 
-
+    return "due o più numeri inseriti sono uguali";
 
 }
 
+function stampaRisultato(risultato: string): void {
+    console.log(risultato);
+}
 
 let input1: number = prompt("inserisci il primo numero") * 1;
 let input2: number = prompt("inserisci il secondo numero") * 1;
 let input3: number = prompt("inserisci il terzo numero") * 1;
-numeroMaggiore(input1, input2, input3)
-
+stampaRisultato(numeroMaggiore(input1, input2, input3));
