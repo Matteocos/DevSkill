44bdbd8a280a9cd414f202372154942b99b73fa0
Matteo Cosenza <matteocosenza12@gmail.com>
2022-04-21 17:08:45 +0200

    Aggiunto esercizio funzioni 1

--- eserc_funz_1.ts
+++ eserc_funz_1.ts
@@ -1,16 +1,12 @@
 function posNeg(num: number): string {
-    if (num < 0) {
-        return console.log("il numero inserito:" + " " + num + "  è negativo.");
+    if (num >= 0) {
+        return "il numero inserito:" + " " + num + "  è positivo.";
     }
-    if (num == 0) {
-        return console.log("il numero inserito è 0");
-    }
-    if (num > 0) {
-        return console.log("il numero inserito:" + " " + num + "  è positivo.");
-    }
+    return "il numero inserito:" + " " + num + "  è negativo.";
+
 }
 
 
-let input: number = prompt("inserisci un numero");
+let input: number = prompt("inserisci un numero") * 1;
+console.log(posNeg(input));
 
-posNeg(input)
