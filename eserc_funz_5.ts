6fe885f26131fddfbc5fce98ce47d53500f6e6fc
Matteo Cosenza <matteocosenza12@gmail.com>
2022-04-21 17:09:44 +0200

    Aggiunto esercizio funzioni 5

--- eserc_funz_5.ts
+++ eserc_funz_5.ts
@@ -2,11 +2,14 @@ function annoBisestile(anno: number): string {
 
 
     if ((anno % 400 == 0) || (anno % 4 == 0 && anno % 100 != 0)) {
-        return console.log("L'anno inserito è bisestile");
+        return "L'anno inserito è bisestile";
     }
-    else {
-        return console.log("L'anno inserito non è bisestile");
-    }
+
+    return "L'anno inserito non è bisestile";
+
+}
+function stampaRisultato(risultato: string): void {
+    console.log(risultato);
 }
 let annocorrente: number = prompt("insersci l'anno") * 1;
-annoBisestile(annocorrente);
+stampaRisultato(annoBisestile(annocorrente));
