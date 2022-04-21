192b74d40588a05fc830525cae96bc18a53904b7
Matteo Cosenza <matteocosenza12@gmail.com>
2022-04-21 17:09:31 +0200

    Aggiunto esercizio funzioni 4

--- eserc_funz_4.ts
+++ eserc_funz_4.ts
@@ -1,16 +1,18 @@
 function eta(input: number): string {
     if (input < 2022) {
-        console.log("La tua eta è: " + (2022 - input) + " anni")
+        return "La tua eta è: " + (2022 - input) + " anni";
     }
     else if (input > 2022) {
-        console.log(" O hai sbagliato ad inserire il numero, o vieni dal futuro ," + "nel caso tu venga dal futuro, nascerai tra : " + (- (2022 - input)) + " anni"
+        return " O hai sbagliato ad inserire il numero, o vieni dal futuro ," + "nel caso tu venga dal futuro, nascerai tra : " + (- (2022 - input)) + " anni";
     }
-    else {
-        console.log("Sei nato quest'anno")
-    }
-
+    return "Sei nato quest'anno";
 }
 
 
-let input: number = prompt("inserisci l'anno di nascita")
-eta(input)
+
+function stampaRisultato(risultato: string): void {
+    console.log(risultato);
+}
+
+let input: number = prompt("inserisci l'anno di nascita");
+stampaRisultato(eta(input));
